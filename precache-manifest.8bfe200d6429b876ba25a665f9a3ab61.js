54917d5c4d649dcba8a433164279202feaa706de
fsolarv22 <fsolarv@gmail.com>
2020-10-28 16:02:45 -0300

    Updates

---
+++ precache-manifest.8bfe200d6429b876ba25a665f9a3ab61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b5996b852d01c4c39076fe7ee4bc3a",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "2120dd3b80c5e44070e3",
+    "url": "/box-office/static/css/main.57651e31.chunk.css"
+  },
+  {
+    "revision": "fe0009c4d8130d936365",
+    "url": "/box-office/static/js/2.b6690efa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.b6690efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2120dd3b80c5e44070e3",
+    "url": "/box-office/static/js/main.d044e00b.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
